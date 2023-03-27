9f34e90c5c942182eab09ea48c563df69a9b5314
farhan <farhan@cloudsource.io>
2023-03-28 01:56:27 +0500

    implemented location based search functionality

--- event_tickets/models.go
+++ event_tickets/models.go
@@ -3,13 +3,11 @@ package event_tickets
 import (
 	"event_ticket_service/dataservice"
 	"github.com/gin-gonic/gin"
-	"github.com/olivere/elastic/v7"
 )
 
 type Server struct {
 	store       dataservice.Store
 	router      *gin.Engine
 	routerGroup *gin.RouterGroup
-	elastic 	*elastic.Client
 }
 
